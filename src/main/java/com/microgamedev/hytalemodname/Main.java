abe3dc1f760b8bb03bd4831984b0c7694b846a46
ABK <89964469+abkofficial@users.noreply.github.com>
2026-03-28 00:23:43 +0530

    Base Hytale Mod Template

---
+++ src/main/java/com/microgamedev/hytalemodname/Main.java
@@ -0,0 +1,20 @@
+package com.microgamedev.hytalemodname;
+
+
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class Main extends JavaPlugin {
+
+    public Main(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+}
